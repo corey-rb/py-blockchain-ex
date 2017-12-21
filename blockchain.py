7c0a1805d71abcbf0cd8169d1572417550d424a5
Corey Schaf <cschaf@gmail.com>
2017-12-20 22:06:16 -0500

    mining

--- blockchain.py
+++ blockchain.py
@@ -101,6 +101,22 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
+    # we run the proof of work algorithm to get the next proof
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # we must recieve a reward for finding the proof.
+    # the sender is "0" to signify that this node has mined a new coin
+    blockchain.new_transaction(
+        sender = "0",
+        recipient = node_identifier,
+        amount = 1
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+    
     return "We'll mine a new block"
 
 @app.route('/transactions/new', methods=['POST'])
@@ -114,6 +130,8 @@ def full_chain():
         'length': len(blockchain.chain),
     }
     return jsonify(response), 200
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
